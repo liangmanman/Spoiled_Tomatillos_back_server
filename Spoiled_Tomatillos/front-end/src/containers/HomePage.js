74abbc13f30a1b716d99f74155321d20b8b69ec6
Sebastian Wisniowiecki <Sebastian Wisniowiecki>
2018-02-22 16:03:20 -0500

    Not finished ,initial commits

--- Spoiled_Tomatillos/front-end/src/containers/HomePage.js
+++ Spoiled_Tomatillos/front-end/src/containers/HomePage.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom'
-import { MOVIE_LIST_URI } from "./routesContainer/uriConstants";
+import {MOVIE_LIST_URI, SIGNUP_URI} from "./routesContainer/uriConstants";
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -17,10 +17,31 @@ class HomePage extends React.Component {
     render() {
         return (
             <div>
-                <h2>Home Page</h2>
+                <h2>Spoiled Tomatillos</h2>
                 <Link to={MOVIE_LIST_URI}>Movie List</Link>
+                <br/>
+                <Link to={SIGNUP_URI}>Sign Up</Link>
+                <form onSubmit={this.handleLogin}>
+                    <label>
+                        Username or Email Address:
+                        <br/>
+                        <input type={"text"} name={"name"} />
+                        <br/>
+                    </label>
+                    <label>
+                        Password:
+                        <br/>
+                        <input type={"text"} name={"password"} />
+                        <br/>
+                    </label>
+                    <input type={"submit"} value={"Log In"} />
+                </form>
             </div>
         );
+    }
+
+    handleLogin(event) {
+        alert('user tried to log in!');
     }
 }
 
