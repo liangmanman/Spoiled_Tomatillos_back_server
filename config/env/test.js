b64879b35f32f894ef7daea83bac6db1afbe4779
liangmanman <liang.man@husky.neu.edu>
2018-03-21 11:12:49 -0400

    fix mongodb test uri

--- config/env/test.js
+++ config/env/test.js
@@ -5,6 +5,6 @@
  */
 
 module.exports = {
-    mongodb: 'mongodb://admin:admin@ds121189.mlab.com:21189/spoiled-tomatillos-test',
+    mongodb: 'mongodb://localhost:27017/spoiled-tomatillos-test',
     secret: "Spring is coming",
 };
