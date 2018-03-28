9054f155516e543a5cdbb24e871173ff2eacc5f7
liangmanman <liang.man@husky.neu.edu>
2018-03-28 19:12:34 -0400

    TEAM13-62 #in-development #comment almost done

--- test/index.js
+++ test/index.js
@@ -1,11 +1,42 @@
 const assert = require('assert');
+
+const {
+  updateFriendOrCreateIfNotExist,
+  determineIsFriendOfUser,
+  deleteFriend,
+} = require('../app/module/friends');
+
+const {
+  like,
+  unlike,
+  findMoviesLikedByUserId,
+  findUsersLikeMovieId,
+} = require('../app/module/likes');
+
+const {
+  createMovieIfNotExist,
+} = require('../app/module/movies');
+
+const {
+  updateRateOrCreateIfNotExist,
+  deleteRateByUserIdAndMovieId,
+  findRateQuery,
+  calculateRateOfMovie,
+} = require('../app/module/rates');
+
+const {
+  updateReviewOrCreateIfNotExist,
+  deleteReviewByUserIdAndMovieId,
+  findReviewQuery,
+} = require('../app/module/reviews');
+
 const {
   decodeToken,
   generateJwtTokenForUser,
   verifyMe,
   getUser,
   findUsersBySearch,
-} = require('../app/module/users')
+} = require('../app/module/users');
 
 describe('Array', function() {
   describe('#indexOf()', function() {
@@ -22,7 +53,7 @@ describe('Array', function() {
       assert(generateJwtTokenForUser({userId: "hello"}), 'hello');
     });
     it('verifyMe', function() {
-      assert(verifyMe({userId: "hi"}), 'hello');
+      assert(verifyMe({userId: "hi"}, 'fakeToken'), 'hello');
     });
     it('getUser', function() {
       assert(getUser({userId: "hi"}), 'hello');
@@ -31,5 +62,116 @@ describe('Array', function() {
       assert(findUsersBySearch({searchBy:'fullName'}), 'hello');
     })
 
+  });
+
+  describe('testMovie', function () {
+    it('should createMovieIfNotExist', function () {
+      assert(createMovieIfNotExist({omdbId: 'testid'}), 'testMovie');
+    });
+  });
+
+  describe('testFriend', function () {
+    it('updateFriendOrCreateIfNotExist', function () {
+      assert(updateFriendOrCreateIfNotExist({
+        fromUserId: 'testUserId1',
+        toUserId: 'testUserId2'
+      }), 'testUser');
+    });
+
+    it('determineIsFriendOfUser', function () {
+      assert(determineIsFriendOfUser({
+        fromUserId: 'testUserId1',
+        toUserId: 'testUserId2'
+      }), true);
+    });
+
+    it('deleteFriend', function () {
+      assert(deleteFriend({
+        fromUserId: 'testUserId1',
+        toUserId: 'testUserId2'
+      }), 'testResult');
+    });
+  });
+
+  describe('testLike', function () {
+    it('like', function () {
+      assert(like({
+        userId: 'testUserId',
+        movieId: 'testMovieId',
+      }), 'testResult');
+    });
+
+    it('unlike', function () {
+      assert(unlike({
+        userId: 'testUserId',
+        movieId: 'testMovieId',
+      }), 'testResult');
+    });
+
+    it('findMoviesLikedByUserId', function () {
+      assert(findMoviesLikedByUserId({
+        imdbId: 'testImdbId',
+      }), []);
+    });
+
+    it('findUsersLikeMovieId', function () {
+      assert(findUsersLikeMovieId({
+        userId: 'testUserId'
+      }), [])
+    });
+  });
+
+  describe('testRate', function () {
+    it('updateRateOrCreateIfNotExist', function () {
+      assert(updateRateOrCreateIfNotExist({
+        movieId: 'testMovieId',
+        userId: 'testUserId',
+        rate: 5,
+      }), 'testResult');
+    });
+
+    it('deleteRateByUserIdAndMovieId', function () {
+      assert(deleteRateByUserIdAndMovieId({
+        movieId: 'testMovieId',
+        userId: 'testUserId',
+      }), 'testResult');
+    });
+
+    it('findRateQuery', function () {
+      assert(findRateQuery({
+        movieId: 'testMovieId',
+        userId: 'testUserId',
+      }), 'testResult');
+    });
+
+    it('calculateRateOfMovie', function () {
+      assert(calculateRateOfMovie({
+        movieId: 'testMovieId',
+      }), 'result');
+    });
+  });
+
+  describe('testReview', function () {
+    it('updateReviewOrCreateIfNotExist', function () {
+      assert(updateReviewOrCreateIfNotExist({
+        movieId: 'testMovieId',
+        userId: 'testUserId',
+        contest: 'testContent',
+      }), 'testResult');
+    });
+
+    it('deleteReviewByUserIdAndMovieId', function () {
+      assert(deleteReviewByUserIdAndMovieId( {
+        movieId: 'testMovieId',
+        userId: 'testUserId',
+      }), 'testRestlt');
+    });
+
+    it('findReviewQuery', function () {
+      assert(findReviewQuery({
+        movieId: 'testMovieId',
+        userId: 'testUserId',
+      }), 'testResult');
+    });
   });
 });
