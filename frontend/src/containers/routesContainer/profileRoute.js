cffc0d58cbbdac01644f4a431565cb04be427ddf
liangmanman <liang.man@husky.neu.edu>
2018-04-15 23:00:11 -0400

    TEAM13-76 #in-development finish

--- frontend/src/containers/routesContainer/profileRoute.js
+++ frontend/src/containers/routesContainer/profileRoute.js
@@ -4,27 +4,31 @@ import { observer } from 'mobx-react';
 
 import MyLikesPage from '../UserLikesPage';
 import ProfilePage from '../ProfilePage';
-import { USER_URI, USER_PROFILE_URI, USER_LIKES_URI} from "./uriConstants";
+import FriendsPage from  '../FriendsPage';
+import { USER_URI, USER_PROFILE_URI, USER_LIKES_URI, USER_FRIENDS_URI } from "./uriConstants";
 
 @observer
 class ProfileRoute extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <Switch>
-                <Route key={USER_PROFILE_URI}
-                       path={USER_PROFILE_URI}
-                       component={ProfilePage}/>
-                <Route key={USER_LIKES_URI}
-                       path={USER_LIKES_URI}
-                       component={MyLikesPage}/>
-                <Route key="any" path="*" component={ProfilePage}/>
-            </Switch>
-        );
-    }
+  render() {
+    return (
+      <Switch>
+        <Route key={USER_PROFILE_URI}
+               path={USER_PROFILE_URI}
+               component={ProfilePage}/>
+        <Route key={USER_LIKES_URI}
+               path={USER_LIKES_URI}
+               component={MyLikesPage}/>
+        <Route key={USER_FRIENDS_URI}
+               path={USER_FRIENDS_URI}
+               component={FriendsPage}/>
+        <Route key="any" path="*" component={ProfilePage}/>
+      </Switch>
+    );
+  }
 }
 
 
